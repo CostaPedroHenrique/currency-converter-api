d4d111dce9d5368178dd7b1204cad2459ce87a9b
costapedrohenrique <devpedrohenrique@gmail.com>
2025-04-22 00:20:30 -0300

    refactor: add MinValueValidator to source_amount and target_amount fields in Transaction model

--- apps/transactions/models/transaction.py
+++ apps/transactions/models/transaction.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from apps.users.models import User
@@ -17,9 +18,13 @@ class Transaction(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     source_currency = models.CharField(max_length=3, choices=SUPPORTED_CURRENCIES)
-    source_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    source_amount = models.DecimalField(
+        max_digits=12, decimal_places=2, validators=[MinValueValidator(0)]
+    )
     target_currency = models.CharField(max_length=3, choices=SUPPORTED_CURRENCIES)
-    target_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    target_amount = models.DecimalField(
+        max_digits=12, decimal_places=2, validators=[MinValueValidator(0)]
+    )
     conversion_rate = models.DecimalField(max_digits=12, decimal_places=6)
     date = models.DateTimeField(auto_now_add=True)
 
